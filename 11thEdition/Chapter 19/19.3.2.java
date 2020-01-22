29f0cafbc58fcfc8457ee3d88c467ab865d3c57c
XIAOLONG LI <1491skdpf89@gmail.com>
2020-01-22 15:38:50 -0500

    Update 19.3.2.java

--- 11thEdition/Chapter 19/19.3.2.java
+++ 11thEdition/Chapter 19/19.3.2.java
@@ -1,2 +1,5 @@
 /*Since you create an instance of ArrayList of strings using new ArrayList<String>(), should the constructor in the ArrayList class be defined as*/
 public ArrayList<E>()
+
+  
+/* NO */
