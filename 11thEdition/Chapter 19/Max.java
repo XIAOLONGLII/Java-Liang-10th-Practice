52889276283f9b682a143dc6ad21972ebf077fa7
XIAOLONG LI <1491skdpf89@gmail.com>
2020-01-22 16:38:52 -0500

    Update Max.java

--- 11thEdition/Chapter 19/Max.java
+++ 11thEdition/Chapter 19/Max.java
@@ -3,12 +3,22 @@ class Max{
 		Integer a = new Integer(1);
 		Integer b = new Integer(2);
 		System.out.println(max(a, b));
+		System.out.println("This " + max1(a, b));
 	}
 	public static Comparable max(Comparable o1, Comparable o2) {
 		if(o1.compareTo(o2) > 0) {
 			return o1;
 		}
 		else{
+			return o2;
+		}
+	}
+
+	public static <E extends Comparable> E max1(E o1, E o2) {
+		if(o1.compareTo(o2) > 0) {
+			return o1;
+		} 
+		else {
 			return o2;
 		}
 	}
