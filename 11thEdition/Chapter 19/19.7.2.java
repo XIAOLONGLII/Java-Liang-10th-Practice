e6c98b09b10806c64348472e569254ff48ec0473
XIAOLONG LI <1491skdpf89@gmail.com>
2020-01-22 16:59:28 -0500

    Create 19.7.2.java

---
+++ 11thEdition/Chapter 19/19.7.2.java
@@ -0,0 +1,6 @@
+/*
+What are an unbounded wildcard, a bounded wildcard, and a lower-bound wildcard?
+? is unbounded wildcard
+? extends T is bounded wildcard
+? super T is lower bounded wildcard
+*/
