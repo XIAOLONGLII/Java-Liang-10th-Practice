74cb136f2c130b3271ff260fffcd18adc8b185dc
XIAOLONG LI <1491skdpf89@gmail.com>
2020-01-22 16:58:36 -0500

    Create 19.7.1.java

---
+++ 11thEdition/Chapter 19/19.7.1.java
@@ -0,0 +1,8 @@
+/*
+
+Is GenericStack the same as GenericStack<Object>?
+
+
+GenericStack is roughly equivalent to GenericStack<Object>, but they are not the same. GenericStack<Object> is a generic instantiation, but GenericStack is a raw type.
+
+*/
