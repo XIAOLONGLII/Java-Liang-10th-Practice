7f9c54d21cf78c401776d74dc4d4816848875884
XIAOLONG LI <1491skdpf89@gmail.com>
2020-01-22 16:10:48 -0500

    Create 19.4.1.java

---
+++ 11thEdition/Chapter 19/19.4.1.java
@@ -0,0 +1,9 @@
+/* 
+How do you declare a generic method? How do you invoke a generic method?
+
+
+To declare a generic method, you place the generic type <E> immediately after the keyword static in the method. 
+A generic method can be invoked just like a regular method. The compiler automatically discovers the actual type.
+
+
+*/
