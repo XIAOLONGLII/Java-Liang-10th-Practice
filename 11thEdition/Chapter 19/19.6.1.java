d2e881d5637f720fb4dee7ba906ca5b6c4f599e5
XIAOLONG LI <1491skdpf89@gmail.com>
2020-01-22 16:51:49 -0500

    Create 19.6.1.java

---
+++ 11thEdition/Chapter 19/19.6.1.java
@@ -0,0 +1,10 @@
+/* 
+What is a raw type? Why is a raw type unsafe? Why is the raw type allowed in Java?
+
+
+When you use generic type without specifying an actual parameter, it is called a raw type. 
+A raw type is unsafe, because some errors cannot be detected by the compiler. 
+The raw type is allowed in Java for backward compatibility.
+
+
+*/
