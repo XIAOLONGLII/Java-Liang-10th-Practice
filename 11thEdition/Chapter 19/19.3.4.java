2b6c0d7d9dac22bd15057b0b3c3a25c59e04257c
XIAOLONG LI <1491skdpf89@gmail.com>
2020-01-22 15:40:46 -0500

    Create 19.3.4.java

---
+++ 11thEdition/Chapter 19/19.3.4.java
@@ -0,0 +1,4 @@
+/*How do you declare a generic type in a class? */
+
+
+/*To declare a generic type for a class, place the generic type after the class name, such as GenericStack<E>. To declare a generic type for a method, place the generic type for the method return type, such as <E> void max(E o1, E o2). */
