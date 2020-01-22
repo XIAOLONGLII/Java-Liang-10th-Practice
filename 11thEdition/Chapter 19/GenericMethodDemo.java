fbbf04a4b568b140e7ffa9b1b1d36992ad86cfba
XIAOLONG LI <1491skdpf89@gmail.com>
2020-01-22 15:47:04 -0500

    Create GenericMethodDemo.java

---
+++ 11thEdition/Chapter 19/GenericMethodDemo.java
@@ -0,0 +1,16 @@
+class GenericMethodDemo {
+	public static void main(String[] args) {
+		Integer[] integers = {1, 2, 3, 4, 5};
+		String[] strings = {"hello", "world", "I love", "you"};
+
+		GenericMethodDemo.<Integer>print(integers);
+		GenericMethodDemo.<String>print(strings);
+
+	}
+	public static <E> void print(E[] list) {
+		for(int i = 0; i < list.length; i++){
+			System.out.println(list[i] + " ");
+		}
+		System.out.println();
+	}
+}
